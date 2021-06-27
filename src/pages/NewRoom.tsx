eac8b3b21d0904031b991bc43f3804b68e658666
allyson7 <allysonlopes0@gmail.com>
2021-06-27 12:59:34 -0400

    FEAT: Authentication hook

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -5,12 +5,11 @@ import logoImg from "../assets/images/logo.svg";
 
 import '../styles/auth.scss';
 import { Button } from "../components/Button";
-import { useContext } from 'react';
-import { AuthContext } from '../App';
+import { useAuth } from '../hooks/useAuth';
 //webpack (snowpack, vite, ...)
 
 export function NewRoom() {
-  const { user } = useContext(AuthContext);
+  const { user } = useAuth();
 
   return (
     <div id="page-auth">
@@ -22,7 +21,6 @@ export function NewRoom() {
       <main>
         <div className="main-content">
           <img src={logoImg} alt="Letmeask" />
-          <h1>{user?.name}</h1>
           <h2>Criar uma nova sala</h2>
           <form>
             <input
