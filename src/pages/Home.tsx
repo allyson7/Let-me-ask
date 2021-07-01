65dbae75ac4a5bf5dbd5fc0231165c586a61c8ac
allyson7 <allysonlopes0@gmail.com>
2021-07-01 18:02:23 -0400

    little fix update

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,10 +5,13 @@ import illustrationImg from "../assets/images/illustration.svg";
 import logoImg from "../assets/images/logo.svg";
 import googleIconImg from "../assets/images/google-icon.svg";
 
-import '../styles/auth.scss';
+import { database } from '../services/firebase';
+
 import { Button } from "../components/Button";
 import { useAuth } from '../hooks/useAuth';
- //webpack (snowpack, vite, ...)
+//webpack (snowpack, vite, ...)
+
+import '../styles/auth.scss';
 
 export function Home() {
   const history = useHistory();
@@ -24,7 +27,20 @@ export function Home() {
   }
 
   async function handleJoinRoom(event: FormEvent) {
-    event
+    event.preventDefault();
+
+    if (roomCode.trim() === '') {
+      return;
+    }
+
+    const roomRef = await database.ref(`rooms/${roomCode}`).get();
+
+    if (!roomRef.exists()) {
+      alert('Roomdoes not exists.');
+      return;
+    }
+
+    history.push(`/rooms/${roomCode}`);
   }
 
   return (
